dfa11c33ea18a75725f725f0448e3daf9975c767
Yoko Kamata <koitn.saru@gmail.com>
2021-09-17 22:44:44 +0900

    #2終了

--- vueStudy/js/main.js
+++ vueStudy/js/main.js
@@ -1,6 +1,7 @@
 (function() {
 	'use strict';
 
+	// ========== #2 ==========
 	// データを更新するとUIが更新される(その逆も然り)
 	// two way data binding (to UI)
 	var vm = new Vue({
@@ -9,4 +10,5 @@
 			name: 'taguchi'
 		}
 	});
+	// ========== /#2 ==========
 })();
