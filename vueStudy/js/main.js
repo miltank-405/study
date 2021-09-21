71acb72065d24c866215e91a592338a5f88788be
Yoko Kamata <koitn.saru@gmail.com>
2021-09-21 21:33:22 +0900

    js : varからletに変更 html : コメント追加

--- vueStudy/js/main.js
+++ vueStudy/js/main.js
@@ -4,7 +4,7 @@
 	// ========== #2 ==========
 	// データを更新するとUIが更新される(その逆も然り)
 	// two way data binding (to UI) -- 双方向データバインディング
-	var vm = new Vue({
+	let vm = new Vue({
 		el: '#app',
 		data: {
 			newItem: '',
