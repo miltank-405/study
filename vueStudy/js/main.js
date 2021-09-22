0f8563e67ba5ba310009f1f51d0ef29a39557559
Yoko Kamata <koitn.saru@gmail.com>
2021-09-22 15:02:30 +0900

    算出プロパティ, filter

--- vueStudy/js/main.js
+++ vueStudy/js/main.js
@@ -19,6 +19,16 @@
 				if (confirm('are you sure?'))
 					this.todos.splice(index, 1);
 			}
+		},
+		// todoの残数を表示していく
+		// computedはデータから動的にプロパティを計算してくれる算出プロパティ
+		computed: {
+			remaining: function() {
+				let items = this.todos.filter(function(todo){
+					return !todo.isDone;
+				});
+				return items.length;
+			}
 		}
 	});
 })();
