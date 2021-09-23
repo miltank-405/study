ca6af46267b2c47ced567844eb548f47380e7e27
Yoko Kamata <koitn.saru@gmail.com>
2021-09-23 21:20:07 +0900

    props, デフォルト値や型の指定

--- vueStudy/lesson_14after/main.js
+++ vueStudy/lesson_14after/main.js
@@ -2,14 +2,20 @@
 	'use strict';
 
 	// Components
-
 	let likeComponent = Vue.extend({
+		// props: ['message'],
+		props: {
+			message: {
+				type: String,
+				default: 'Like'
+			}
+		},
 		data: function() {
 			return {
 				count: 0
 			}
 		},
-		template: '<button @click="countUp">Like {{ count }} </button>',
+		template: '<button @click="countUp">{{ message }} {{ count }}</button>',
 		// 複数の要素を含める場合は、何らかの親要素で囲ってあげる必要がある
 		// template: '<div><button>Like</button><button>Like</button></div>'
 		methods: {
